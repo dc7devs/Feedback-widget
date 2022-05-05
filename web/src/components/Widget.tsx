f80c1d815d3d7f2f17f0ed3a3f7bf7f642bbfb71
Diego Silva <diegocaetano444@outlook.com>
2022-05-05 00:31:51 -0300

    [☕] - implementing popover using heads-ui

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,25 +1,21 @@
-import { ChatsTeardrop, ToggleLeft } from "phosphor-react";
-import { useState } from "react";
+import { ChatsTeardrop } from "phosphor-react";
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false);
-
-    function toggleWidgetVisibility() {
-        setIsWidgetOpen(!isWidgetOpen)
-    }
-
     return (
-        <div className="fixed bottom-6 right-6">
-            { isWidgetOpen && <p>Hello Word</p> }
+        <Popover className="fixed bottom-6 right-6">
+            <Popover.Panel>
+                Hello
+            </Popover.Panel>
 
-            <button onClick={toggleWidgetVisibility} className="flex bg-brand-500 rounded-full px-3 h-12 text-white items-center group">
+            <Popover.Button className="flex bg-brand-500 rounded-full px-3 h-12 text-white items-center group">
                 <ChatsTeardrop className="w-6 h-12"/>
 
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear"> 
                     <span className="pl-2"></span>
                     Feedback
                 </span>
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     );
 }
