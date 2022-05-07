4cca6dbe0bbc83222ae758d5e49d4e02903249eb
Diego Silva <diegocaetano444@outlook.com>
2022-05-07 02:53:19 -0300

    [📬] - Implemented email sending

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "9b04dacd9feb76",
+      pass: "d38436e4fbfec3"
+    }
+});
 
 app.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
             comment,
             screenshot,
         }
+    });
+
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi@feeedget.com>',
+        to: 'Diego Silva <diegocatano444@outlook.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`
+        ].join('\n')
     });
 
     return res.status(201).json({ data: feedback });
